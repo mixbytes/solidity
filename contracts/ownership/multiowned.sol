ceafb8d7e16387693d39fa717adfd2381ae390f4
Eenae <z-line@my.com>
2017-10-19 12:16:25 +0300

    added disclaimers

--- contracts/ownership/multiowned.sol
+++ contracts/ownership/multiowned.sol
@@ -1,3 +1,12 @@
+// Copyright (C) 2017  MixBytes, LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND (express or implied).
+
 // Code taken from https://github.com/ethereum/dapp-bin/blob/master/wallet/wallet.sol
 // Audit, refactoring and improvements by github.com/Eenae
 
